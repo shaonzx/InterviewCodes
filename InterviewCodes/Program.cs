e0f8bb49b11a5162aefc4ce5c8267f18281af198
Shaon <shaon.cpp@gmail.com>
2022-06-16 18:55:40 +0600

    Add project files.

---
+++ InterviewCodes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Blah");
+        }
+    }
+}
